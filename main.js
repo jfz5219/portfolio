08435840405ec574b6f36ee727a6fe5291d0c7ea
jfz5219 <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>
2025-01-23 21:48:38 -0500

    setup portfolio

---
+++ main.js
@@ -0,0 +1,25 @@
+emailjs.init('n-9rzJ1kCzdbVInZK');
+
+const form = document.getElementById('contactForm');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(document.getElementById('name').value)
+    // Get form data
+    const formData = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value,
+    };
+
+    // Send email using EmailJS
+    emailjs.send('service_o8yjvzk', 'template_u9kvyna', formData)
+        .then(() => {
+            alert('Message sent successfully!');
+            form.reset();
+        })
+        .catch((error) => {
+            console.error('Error sending email:', error);
+            alert('Oops! Something went wrong. Please try again.');
+        });
+});

11292955c96b852caefc1bb84cd32b0d57bede93
jfz5219 <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>
2025-01-30 16:41:11 -0500

    move images to aws

--- pages/gallery.js
+++ pages/gallery.js
@@ -12,8 +12,8 @@ if (page_name == "roomshare") {
 
 
 const images = Array.from({ length: image_length }, (_, i) => ({
-    src: `/pages/fullsize_${page_name}/${i + 1}.webp`,
-    thumb: `/pages/thumbnail_${page_name}/${i + 1}.webp`,
+    src: `https://jxz-portfolio-images.s3.us-east-1.amazonaws.com/projects/${page_name}/fullsize/${i + 1}.webp`,
+    thumb: `https://jxz-portfolio-images.s3.us-east-1.amazonaws.com/projects/${page_name}/thumbnail/${i + 1}.webp`,
 }));
 
 // create gallery
